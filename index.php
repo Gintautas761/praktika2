a5f0c73e0c500d1aacf45dd0d6b2e07b221b5381
Gintautas Simulis <gintautas.simulis@panko.lt>
2021-03-23 12:17:50 +0200

    sukuriamas failas ir perduodama informacija

--- index.php
+++ index.php
@@ -1,1 +1,44 @@
-labas
+<?php
+
+
+
+$data = array
+(
+    [
+    'first_name' => 'Kiestis',
+    'age' => 29,
+    'gender' => 'male'
+    ],
+    [
+    'first_name' => 'Vytska',
+    'age' => 32,
+    'gender' => 'male'
+    ],
+    [
+    'first_name' => 'Karina',
+    'age' => 25,
+    'gender' => 'female'
+    ],
+);
+
+    print_r($data);
+
+    if (file_exists("failas.txt")) {
+        $file = "failas.txt";
+        $current = file_get_contents($file);
+    } else {
+        $myfile = fopen("failas.txt","w");
+        header("refresh:0");         
+    }
+
+?>
+<form action="process.php" method="post">
+    <textarea rows="20" cols="50" name="comment">
+        <?php
+        echo $current;
+        ?>
+    </textarea>
+
+    <input type="submit">
+
+</form>
---
+++ process.php
@@ -0,0 +1,9 @@
+<?php
+$comment = $_POST["comment"];
+$file = "failas.txt";
+file_put_contents($file, $comment);
+header('Location:Http://localhost/');
+
+
+
+?>
